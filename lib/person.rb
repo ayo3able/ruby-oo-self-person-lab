1c433b8f8425a975f9a6b84fd3c1368bbc79e49d
ayo3able <mrsbarclay101008@gmail.com>
2020-10-18 14:47:27 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,14 @@
 # your code goes here
+class Person
+    attr_accessor :bank_account, :happiness, :hygiene
+    attr_reader :name
+    def initialize(name)
+        @name = name
+     end
+     def initialize(bank_account)
+     @bank_account = bank_account = 25
+       
+    end
+  
+end
+
